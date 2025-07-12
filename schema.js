124c2bc54cafbc1a11ec4908ad48f7236b0145ab
yadavi123-art <yadavayushi@gmail.com>
2025-07-12 20:15:05 +0530

    booking API completed

--- schema.js
+++ schema.js
@@ -80,7 +80,14 @@ const bookingSchema = new mongoose.Schema({
   user_id: { type: String, required: true },
   hall_id: { type: String, required: true },
   booking_dates: { type: [Date], required: true },
-  status: { type: String, enum: ['booked', 'canceled'], required: true }
+  status: { type: String, enum: ['booked', 'canceled'], required: true },
+  purpose: { type: String, required: true },
+  guest_quantity: { type: Number, required: true },
+  addons: [{ type: String }],
+  special_requests: { type: String },
+  contact_name: { type: String, required: true },
+  contact_email: { type: String, required: true },
+  contact_phone: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Booking', bookingSchema);
